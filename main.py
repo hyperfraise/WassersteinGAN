0a3932baf81938769632817b5f8c0f63617dad3f
hyperfraise <damien.menigaux@polytechnique.edu>
2022-02-28 11:31:59 +0100

    s

--- main.py
+++ main.py
@@ -299,13 +299,13 @@ if __name__ == "__main__":
             netG.parameters(), lr=0.0005, betas=(opt.beta1, 0.999)
         )
         optimizerG_smoothing = optim.Adam(
-            netG.parameters(), lr=0.0005, betas=(opt.beta1, 0.999)
+            netG.parameters(), lr=0.0001, betas=(opt.beta1, 0.999)
         )
     else:
         optimizerD = optim.RMSprop(netD.parameters(), lr=opt.lrD)
         optimizerG = optim.RMSprop(netG.parameters(), lr=opt.lrG)
         optimizerG_fixed = optim.RMSprop(netG.parameters(), lr=0.0005)
-        optimizerG_smoothing = optim.RMSprop(netG.parameters(), lr=0.0005)
+        optimizerG_smoothing = optim.RMSprop(netG.parameters(), lr=0.0001)
 
     fixed_input_criterion = nn.MSELoss().cuda()
     siamese_criterion = nn.MSELoss().cuda()
